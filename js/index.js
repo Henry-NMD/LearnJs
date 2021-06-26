16fb8c8a65f83616b415da5b2bdda6e8ced7f4c5
NGUYỄN MINH DUY <nguyenminhduy941gmail.com@MacBook-Pro.local>
2021-06-26 08:59:19 +0700

    update fommat

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-
+//check null
+function myFunction() {
+    let x = document.getElementById("id1").value;
+    let y = document.getElementById("frm").value;
+    if (x == null || x == "") {
+        alert("input not null");
+        document.getElementById("id1").focus();
+    } else {
+        alert(y);
+    }
+}
+//check null to focus
+function upperCase() {
+    let x = document.getElementById("id1");
+    if (x.value == "") {
+        alert("please input!");
+        document.getElementById("id1").focus();
+        
+    }
+    x.value = x.value.toUpperCase();
+}
+//only reset from form
+function reset() {
+    document.getElementById("frm").reset();
+}

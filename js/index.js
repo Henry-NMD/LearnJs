58bff3750284cfd1d74db5f9e8ab0ee433b56cb4
NGUYỄN MINH DUY <nguyenminhduy941gmail.com@MacBook-Pro.local>
2021-06-26 15:48:16 +0700

    update list

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 //check null
 function myFunction() {
     let x = document.getElementById("id1").value;
-    let y = document.getElementById("frm").value;
     if (x == null || x == "") {
         alert("input not null");
         document.getElementById("id1").focus();
     } else {
-        alert(y);
+        document.getElementById("demo").innerHTML = x;
+        //document.getElementById("id2").value  = x; //set value from input
     }
 }
 //check null to focus
@@ -22,4 +22,22 @@ function upperCase() {
 //only reset from form
 function reset() {
     document.getElementById("frm").reset();
+}
+var x = 0;
+var array = [];
+function list() {
+    var e = "<hr/>";   
+    let i  = document.getElementById("id1").value;
+    let i2 = document.getElementById("id2").value;
+    array.push(i);
+    array.push(i2);
+    //array[x] = i;
+    x++;
+   document.getElementById("id1").value = "";
+   for (let y = 0 ; y < array.length ;y++) {
+       e += array[y] + " ";      
+       document.getElementById("demo").innerHTML = e;
+
+   }
+
 }
